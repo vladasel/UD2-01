90c0761f6b8cfdb26a7872fda35560dad62fb7e0
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-07 09:52:47 +0200

    done

---
+++ UD2/src/ComentarioVariasLineasApp.java
@@ -0,0 +1,15 @@
+
+public class ComentarioVariasLineasApp {
+
+	public static void main(String[] args) {
+		/*
+		 * salkdfjhsdkughfskjghksufdhg sfdgfdhdg
+		 */
+		for (int i = 0; i < 5; /* sedgfsdg */ i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println("fin programa");
+
+	}
+
+}
---
+++ UD2/src/HolaMundoApp.java
@@ -0,0 +1,11 @@
+
+public class HolaMundoApp {
+
+	public static void main(String[] args) {
+		// System.out.println("Hola");
+		System.out.println("Adiooooos");
+		System.out.println("FIN PROGRAMA ;)");
+
+	}
+
+}
